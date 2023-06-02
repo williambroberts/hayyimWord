499cb927fb550d406020d1c9d968a5df73dd574f
= <=3williamroberts@gmail.com>
2023-06-02 09:01:31 +0100

    r 9

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -52,6 +52,8 @@ const Header = () => {
     myHtml.style.overflowY="hidden"
   }else{
     myHtml.style.overflowY="scroll"
+    setIsChaptersMenuOpen(false)
+    setIsVersesMenuOpen(false)
   }
 
  },[isHamburger])
@@ -64,7 +66,7 @@ const Header = () => {
     >{chaptersAndVerses[displayTitle[0]].name} {displayTitle[1]+1}</span> : <span className='header-book'> {pathname.slice(1,2).toUpperCase()}{pathname.slice(2).toLowerCase()}</span>}
 
     <span className='header-theme'><ThemeButton/></span>
-    <span className=''><ReactThemeButton/></span>
+    {/* <span className=''><ReactThemeButton/></span> */}
     <span className='header-search' onClick={()=>setIsSearch((prev)=>true)}><IconMagnify/></span>
     </nav>
     <div className={`hamburger-blur ${isHamburger? 'open':''} ${isSearch? "open":""}`} onClick={()=>handleMenu()}></div>
